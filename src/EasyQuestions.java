12fb4705d60f2dcd00148a65890d1a4a878bf6fb
Brian DeLeonardis <Brian DeLeonardis>
2016-07-16 14:02:02 -0400

    Solved up to problem10.

--- src/EasyQuestions.java
+++ src/EasyQuestions.java
@@ -4,7 +4,27 @@ public class EasyQuestions
 	//Just a driver/tester
 	public static void main(String[] args)
 	{
-		System.out.println(sumSquareDiff(100));
+		/*String quest8 = "73167176531330624919225119674426574742355349194934"
+				+ "96983520312774506326239578318016984801869478851843"
+				+ "85861560789112949495459501737958331952853208805511"
+				+ "12540698747158523863050715693290963295227443043557"
+				+ "66896648950445244523161731856403098711121722383113"
+				+ "62229893423380308135336276614282806444486645238749"
+				+ "30358907296290491560440772390713810515859307960866"
+				+ "70172427121883998797908792274921901699720888093776"
+				+ "65727333001053367881220235421809751254540594752243"
+				+ "52584907711670556013604839586446706324415722155397"
+				+ "53697817977846174064955149290862569321978468622482"
+				+ "83972241375657056057490261407972968652414535100474"
+				+ "82166370484403199890008895243450658541227588666881"
+				+ "16427171479924442928230863465674813919123162824586"
+				+ "17866458359124566529476545682848912883142607690042"
+				+ "24219022671055626321111109370544217506941658960408"
+				+ "07198403850962455444362981230987879927244284909188"
+				+ "84580156166097919133875499200524063689912560717606"
+				+ "05886116467109405077541002256983155200055935729725"
+				+ "71636269561882670428252483600823257530420752963450";*/
+		System.out.println(sumPrimes(2000000));
 	}
 
 	//-------------------------------------------------------
@@ -178,5 +198,93 @@ public class EasyQuestions
 		tot2 *= tot2;
 		
 		return tot2 - tot1;
+	}
+	
+	//-------------------------------
+	//Question 7
+	//Find the 10,001st prime number
+	//-------------------------------
+	public static int nthPrime(int num)
+	{
+		int count = 0;
+		for(int i = 2;; i++)
+		{
+			if(isPrime(i))
+				count++;
+			
+			if(count == num)
+				return i;
+		}
+		
+	}
+	
+	//--------------------------------------------------------------
+	//Question 8
+	//The four adjacent digits in the 1000-digit number that have 
+	//the greatest product are 9 × 9 × 8 × 9 = 5832.
+	//*1000 digit number commented out in main*
+	//Find the thirteen adjacent digits in the 1000-digit number that 
+	//have the greatest product. What is the value of this product?
+	//---------------------------------------------------------------
+	public static long largeProdinSeries(String num, int numDigits)
+	{
+		long max = 0;
+		for(int i = 0; i + numDigits< num.length(); i++)
+		{
+			long total = 1;
+			int count = 0;
+			for(int j = i; count < numDigits; j++)
+			{
+				total *= Integer.parseInt(num.charAt(j) + "");
+				count++;
+			}
+			
+			if(total > max)
+			{
+				//System.out.println(max);
+				max = total;
+			}
+		}
+		return max;
+	}
+	
+	//------------------------------------------------------------------------------
+	//Question 9
+	//A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
+	//a2 + b2 = c2
+	//For example, 32 + 42 = 9 + 16 = 25 = 52.
+	//There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+	//Find the product abc.
+	//-------------------------------------------------------------------------------
+	public static long specialPythTriplet()
+	{
+		for(int a = 1; a < 500; a++)
+			for(int b = 1; b < 500; b++)
+			{
+				double c = Math.sqrt(a*a + b*b);
+				
+				if((int)c == c)
+					if(a + b + c == 1000)
+						return (long)((long)a*b*c);
+			}
+		
+		return 1;
+	}
+	
+	//-------------------------------------------------------------
+	//Question 10
+	//The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+	//Find the sum of all the primes below two million.
+	//-------------------------------------------------------------
+	public static long sumPrimes(int cap)
+	{
+		long total = 5;
+		for(int i = 5; i < cap; i+=2)
+		{
+			if(isPrime(i))
+				total += i;
+		}
+		
+		return total;
 	}
 }
