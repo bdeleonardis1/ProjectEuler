c9efdc657c3e72e6e68d8e1395dde785ad950013
Brian DeLeonardis <bdeleonardis1@gmail.com>
2016-08-08 16:50:38 -0400

    Solved 35

--- src/ThirtyoneToFourty.py
+++ src/ThirtyoneToFourty.py
@@ -66,5 +66,61 @@ def digit_cancelling_fractions():
 
     return den_tot / gcd
 
-print(digit_cancelling_fractions())
+#-----------
+#Problem 34
+#-----------
+def digit_factorials():
+    answer = 0
+    for i in range(10, 2540161): #upper bound is 9! * 7 (because 9! * 8 is still only 7 digits
+        tot = 0
+        for num in str(i):
+            tot += math.factorial(int(num))
+        if i == tot:
+            answer += i
+    return answer
 
+#------------
+#Problem 35
+#------------
+def circular_primes():
+    count = 0
+    for i in range(2, 1000000):
+        if is_prime(i):
+            l = rotations(i)
+            circular = True
+            for n in l:
+                if(not is_prime(int(n))):
+                    circular = False
+            if circular:
+                count += 1
+    return count
+
+#Helper for 35
+def rotations(s):
+    s = str(s)
+    rotations = []
+    for i in range(1, len(s)):
+        rotations.append(s[(len(s) - i): len(s)] + s[0:(len(s) - i)])
+    return rotations
+
+#Helper for 35
+def is_prime(num):
+    for i in range(2, int(math.sqrt(num)) + 1):
+        if num % i == 0:
+            return False
+    return True
+
+#Random methods to generate all permutations of a string
+def permutation(str):
+    permutations("", str)
+
+#Helper for permutation method
+def permutations(prefix, str):
+    n = len(str)
+    if n == 0:
+        print(prefix)
+    else:
+        for i in range(0, n):
+            permutations(prefix + str[i], str[0:i] + str[i+1:n])
+
+print(circular_primes())
